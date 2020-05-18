1e9dcb772341925ccca6d924f52056736e0953dc
roman.dykyi <roman.dykyi@eglobal-group.com>
2020-05-18 16:12:11 +0300

    fix build in CI

--- application/code/config/bundles.php
+++ application/code/config/bundles.php
@@ -7,5 +7,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     SimpleBus\SymfonyBridge\SimpleBusCommandBusBundle::class => ['all' => true],
-    Symplify\ParameterNameGuard\ParameterNameGuardBundle::class => ['dev' => true, 'test' => true],
+    Symplify\ParameterNameGuard\ParameterNameGuardBundle::class => ['all' => true],
 ];
