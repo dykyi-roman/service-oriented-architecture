9a04409690802837f173dadbb17fcc4713186e05
roman.dykyi <roman.dykyi@eglobal-group.com>
2020-05-18 18:26:58 +0300

    test 1

--- application/code/config/bundles.php
+++ application/code/config/bundles.php
@@ -7,5 +7,4 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     SimpleBus\SymfonyBridge\SimpleBusCommandBusBundle::class => ['all' => true],
-    Symplify\ParameterNameGuard\ParameterNameGuardBundle::class => ['dev' => true, 'test' => true],
 ];
