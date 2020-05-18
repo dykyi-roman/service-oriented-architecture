3eabcb54c73930648d8d2567311751445fe1bcc8
roman.dykyi <roman.dykyi@eglobal-group.com>
2020-05-18 20:23:26 +0300

    test 1

--- application/code/config/bundles.php
+++ application/code/config/bundles.php
@@ -7,4 +7,8 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     SimpleBus\SymfonyBridge\SimpleBusCommandBusBundle::class => ['all' => true],
+    Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
+    Symplify\ParameterNameGuard\ParameterNameGuardBundle::class => ['dev' => true, 'test' => true],
+    Symplify\CodingStandard\SymplifyCodingStandardBundle::class => ['dev' => true, 'test' => true],
+    Symplify\EasyCodingStandard\EasyCodingStandardBundle::class => ['dev' => true, 'test' => true],
 ];
